f7b53f0f3197e90d83d7eb6c8ec9c672c27e9837
Artem Satanovskiy <105995279+ArtSatanov@users.noreply.github.com>
2023-10-17 01:07:11 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { nanoid } from 'nanoid';
+import { ContactList } from './ContactList/ContactList';
+import { FilterBar } from './FilterBar/FilterBar';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = newContact => {
+    if (
+      this.state.contacts.find(
+        option => option.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, { ...newContact, id: nanoid() }],
+      }));
+    }
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = filterValue => {
+    this.setState({
+      filter: filterValue,
+    });
+  };
+
+  render() {
+    const visibleContact = this.state.contacts.filter(contact =>
+      this.state.filter === ''
+        ? contact
+        : contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onAdd={this.addContact} />
+
+        <h2>Contacts</h2>
+        <FilterBar
+          filter={this.state.filter}
+          onChangeFilter={this.changeFilter}
+        />
+        <ContactList contacts={visibleContact} onDelete={this.deleteContact} />
+      </div>
+    );
+  }
+}
